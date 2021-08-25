f3855ef207c478d6a47c3bd3e6a98e729be9a26b
MmmarRTha <m27842n@outlook.com>
2021-08-24 23:18:29 -0700

    update:main - se agrego ejemplo de clase y se mostro en la vista.

--- index.php
+++ index.php
@@ -27,4 +27,31 @@ $task = [
     'completed' => true
     ];
 
+class Task
+{
+    public $description;
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+}
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Finish my screencast'),
+    new Task('Sweep the floor')
+];
+$tasks[0]->complete();
+
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -51,5 +51,17 @@
     </li>
 </ul>
 
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+        <li>
+            <?php if($task->completed) : ?>
+                <strike><?= $task->description; ?></strike>
+            <?php else: ?>
+                <?= $task->description; ?>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
 </body>
 </html>
