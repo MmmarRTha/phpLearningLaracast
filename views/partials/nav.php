f188ca6c7e92d63502374d1e957e6c68552ea515
Martha Maria Nieto Ramirez <marthanietods@nube.unadmexico.mx>
2021-08-29 19:35:58 -0700

    update/main - added a router made from strach that allows to associate any URI endpoint with a controller.

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,6 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Page</a></li>
-        <li><a href="/about/culture">About Culture</a></li>
         <li><a href="/contact">Contact Us</a></li>
     </ul>
 </nav>
